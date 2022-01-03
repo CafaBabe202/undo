52b82842c8326732b77f43eeb6ac3c48e30cc308
WeirdMr <498861397@qq.com>
2022-01-03 08:57:35 +0800

    初始化项目

---
+++ intelligenceBureau/src/main/java/cn/mrwerid/intelligencebureau/IntelligenceBureauApplication.java
@@ -0,0 +1,13 @@
+package cn.mrwerid.intelligencebureau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligenceBureauApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligenceBureauApplication.class, args);
+    }
+
+}
---
+++ intelligenceBureau/src/test/java/cn/mrwerid/intelligencebureau/IntelligenceBureauApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.mrwerid.intelligencebureau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligenceBureauApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
